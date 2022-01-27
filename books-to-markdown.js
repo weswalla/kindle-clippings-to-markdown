d6fe03b1b54eeb6f9586eb926ba87737accb30be
Wesley Finck <wesleyfinck@gmail.com>
2022-01-26 16:52:02 -0800

    refactored code and include note with random highlight

--- books-to-markdown.js
+++ books-to-markdown.js
@@ -4,19 +4,32 @@ const books = require('./store/books.json')
 const { outputDir } = require('./config.json')
 const NEXTCLIP = '\r\n\n---\r\n\n'
 
-for (const title in books) {
-    let markdownContents = ''
-    let bookHighlights = books[title].highlights
-    let bookNotes = books[title].notes
-    for (const noteLocation in bookNotes) {
-        if(noteLocation in bookHighlights) {
-            bookHighlights[noteLocation].content = bookHighlights[noteLocation].content + '\r\n\r\nNOTE:\r\n' + bookNotes[noteLocation].content
+function combineNotesWithHighlights(books) {
+    let combined = {}
+    for (const title in books) {
+        let bookHighlights = books[title].highlights
+        let bookNotes = books[title].notes
+        for (const noteLocation in bookNotes) {
+            if(noteLocation in bookHighlights) {
+                bookHighlights[noteLocation].content = bookHighlights[noteLocation].content + '\r\n\r\nNOTE:\r\n' + bookNotes[noteLocation].content
+            }
+            else {
+                bookNotes[noteLocation].content = 'NOTE:\r\n' + bookNotes[noteLocation].content
+                bookHighlights[noteLocation] = bookNotes[noteLocation]
+            }
         }
-        else {
-            bookNotes[noteLocation].content = 'NOTE:\r\n' + bookNotes[noteLocation].content
-            bookHighlights[noteLocation] = bookNotes[noteLocation]
-        }
+        combined[title] = bookHighlights
     }
+    return combined
+}
+
+let combined = combineNotesWithHighlights(books)
+
+fs.writeFileSync('./store/combined.json', JSON.stringify(combined), 'utf8')
+
+for (const title in combined) {
+    let markdownContents = ''
+    let bookHighlights = combined[title]
     // order the bookhighlights
     let clipLocations = Object.keys(bookHighlights)
     clipLocations.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
--- random_clip.js
+++ random_clip.js
@@ -1,12 +1,29 @@
-const clippings = require('./store/books.json')
+const clippings = require('./store/combined.json')
 
 let titles = Object.keys(clippings)
 
 let randomIndex = Math.floor(Math.random() * titles.length)
 
-let highlights = clippings[titles[randomIndex]].highlights
+let highlights = clippings[titles[randomIndex]]
 let highlightLocations = Object.keys(highlights)
 let randomIndex2 = Math.floor(Math.random() * highlightLocations.length)
 let output = highlights[highlightLocations[randomIndex2]]
+// check if starts with NOTE:, if does include the note before.
+// if doesnt check if it contains NOTE: somewhere in it, if not, check next one if starts with NOTE
+// print both if does, otherwise print just the original one
 console.log(titles[randomIndex])
-console.log(output)
+if(output.content.startsWith('NOTE:')) {
+    console.log(highlights[highlightLocations[randomIndex2 - 1]])
+    console.log(output)
+}
+else if(output.content.includes('NOTE:')) {
+    console.log(output)
+}
+else {
+    console.log(output)
+    if(randomIndex2 < highlightLocations.length - 1) {
+        if(highlights[highlightLocations[randomIndex2 + 1]].content.startsWith('NOTE:')) {
+            console.log(highlights[highlightLocations[randomIndex2 + 1]])
+        }
+    }
+}
